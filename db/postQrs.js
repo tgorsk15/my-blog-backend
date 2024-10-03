66c87317d07b588f3a2770f1b634ad5474c1a715
tgorsk15 <tgbusiness463@gmail.com>
2024-10-02 19:20:07 -0500

    Continue to build out post queries

--- db/postQrs.js
+++ db/postQrs.js
@@ -14,16 +14,66 @@ async function findPostByTitle(title) {
     return post
 }
 
-async function createNewPost(postInfo) {
+async function createNewPost(postInfo, userId) {
     const post = await prisma.post.create({
         data: {
-
+            title: postInfo.title,
+            content: postInfo.content,
+            user: {
+                connect: {
+                    id: userId
+                }
+            }
         }
     })
 }
 
+// TMW 10/3: Build this function, getPublishedPosts function, and whatever else,
+// before moving onto comment queries/controllers
+async function getAllPosts() {
+
+}
+
+async function updatePost(postId, updatedPost) {
+    const postUpdated = await prisma.post.update({
+        where: {
+            id: postId
+        },
+        data: {
+            title: updatePost.title,
+            content: updatePost.content
+        }
+    })
+    console.log('updated post', postUpdated)
+}
+
+async function changePublication(postId, isPublished) {
+    const updatedPost = await prisma.post.update({
+        where: {
+            id: postId
+        },
+        data: {
+            published: isPublished
+        }
+    })
+}
+
+async function removePost(postId) {
+    const removedPost = await prisma.post.delete({
+    where: {
+        id: postId
+    }
+})
+} 
+
+// TMW 10/2: continue to build out some of these functions beforehand...
+// or continue looking into how dat can be retrieved properly from React
+
 
 module.exports = {
     findPostByTitle,
-    createNewPost
+    createNewPost,
+    updatePost,
+    changePublication,
+    removePost
 }
