e4ac49436974deccfeced2f6dfc45ba21ee03cc8
tgorsk15 <tgbusiness463@gmail.com>
2024-10-03 20:27:50 -0500

    Add Passport JWT authentication to create/delete/test comment routes

--- routes/comment.js
+++ routes/comment.js
@@ -1,14 +1,15 @@
 const { Router } = require('express');
 const commentRouter = Router();
 const commentsController = require('../controllers/commentsController')
+const passport = require('passport');
 
-commentRouter.get('/', commentsController.commentTest)
+commentRouter.get('/', passport.authenticate('jwt', {session: false}),commentsController.commentTest)
 
 // post comment
-commentRouter.post('/create/:postId', commentsController.createCommentPost)
+commentRouter.post('/create/:postId', passport.authenticate('jwt', {session: false}),commentsController.createCommentPost)
 
 // delete comment
-commentRouter.delete('/remove/:commentId', commentsController.removeCommentDelete)
+commentRouter.delete('/remove/:commentId', passport.authenticate('jwt', {session: false}),commentsController.removeCommentDelete)
 
 // like comment
 commentRouter.put('/like/:commentId', commentsController.likeCommentPut)
