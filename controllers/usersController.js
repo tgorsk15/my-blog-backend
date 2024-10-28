00c62f9a72b6a685e2ee7641fb297af87325fe44
tgorsk15 <tgbusiness463@gmail.com>
2024-10-28 13:59:21 -0500

    Restructure signupUserPost to include form validation (express-validator)

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -2,6 +2,31 @@ const bcrypt = require('bcryptjs')
 const db = require('../db/userQrs')
 const issueJWT = require('../auth/issueJWT')
 
+const { body, validationResult } = require("express-validator")
+
+const nameErr = "First and Last name both need to be between 2 and 20 characters"
+const usernameErr = "Username must be between 5 and 25 characters"
+const passwordErr = "Password must be at least 7 character long"
+
+const validateUser = [
+    body("firstName").trim()
+        .isLength({ min: 2, max: 20 }).withMessage(nameErr),
+    body("lastName").trim()
+        .isLength({ min: 2, max: 20 }).withMessage(nameErr),
+    body("username").trim()
+        .isLength({ min: 5, max: 25}).withMessage(usernameErr),
+    body("password").trim()
+    .isLength({ min: 7 }).withMessage(passwordErr)
+        .custom((value) => {
+            const specialChar = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
+            if (!specialChar.test(value)){
+                throw new Error("Password must include at least one special character")
+            }
+            return true
+        })
+]
+
+
 exports.userTest = async (req, res) => {
     res.status(200).json({
         success: true,
@@ -15,8 +40,7 @@ exports.loginUserPost = async (req, res) => {
         console.log('given credentials', loginInfo)
         const user = await db.findUserByUsername(loginInfo.username)
 
-        // could add check to see if user.username = loginInfo.username
-        // ... send 'already logged in' msg if needed
+
 
         if (!user) {
             return res.status(401).json({
@@ -53,50 +77,62 @@ exports.loginUserPost = async (req, res) => {
 
 }
 
-exports.signupUserPost = async (req, res) => {
-    try {
-        let isMain = false
-        const info = req.body
+exports.signupUserPost = [
+    validateUser,
+    async (req, res, next) => {
+        try {
+            let isMain = false
+            const info = req.body
+            const errors = validationResult(req);
 
-        // check for repeat
-        const users = await db.getAllUsers();
-        const userExists = users.find(user => user.username === info.username)
-        const emailExists = users.find(user => user.email === info.email)
+            // check for repeat
+            const users = await db.getAllUsers();
+            const userExists = users.find(user => user.username === info.username)
+            const emailExists = users.find(user => user.email === info.email)
 
-        if (userExists) {
-            return res.status(401).json({
-                success: false,
-                msg: 'This username already exists'
+            if (userExists) {
+                return res.status(401).json({
+                    success: false,
+                    msg: 'This username already exists'
+                })
+            } else if (emailExists) {
+                return res.status(401).json({
+                    success: false,
+                    msg: 'This email already exists'
+                })
+            }
+
+            // check for errors in submitted account info:
+            if (!errors.isEmpty()) {
+                console.log('errors with signup:', errors)
+                return res.status(400).json({
+                    success: false,
+                    errors: errors.array()
+                })
+            }
+
+            // hash password and insert into DB
+            const hashedPassword = await bcrypt.hash(info.password, 10);
+            const newUser = await db.insertUser(info, hashedPassword, isMain);
+            
+            // issue the token for user
+            console.log('newUser', newUser)
+            const jwt = await issueJWT.issueToken(newUser, newUser.id)
+            console.log('token issued', jwt)
+
+            res.json({
+                success: true,
+                user: newUser,
+                token: jwt.token,
+                expiresIn: jwt.expires
             })
-        } else if (emailExists) {
-            return res.status(401).json({
-                success: false,
-                msg: 'This email already exists'
-            })
-        }
+        } catch(err) {
+            console.error('Error in signupUserPost:', err);
+            res.status(500).json({ message: 'An error occurred during signup', error: err.message });
+        } 
 
-        // possibly install form validation later with express validator...
-
-        // hash password and insert into DB
-        const hashedPassword = await bcrypt.hash(info.password, 10);
-        const newUser = await db.insertUser(info, hashedPassword, isMain);
-         
-        // issue the token for user
-        console.log('newUser', newUser)
-        const jwt = await issueJWT.issueToken(newUser, newUser.id)
-        console.log('token issued', jwt)
-
-        res.json({
-            success: true,
-            user: newUser,
-            // might not need to return a token... token is issued at login
-            token: jwt.token,
-            expiresIn: jwt.expires
-        })
-    } catch(err) {
-        console.error('Error in signupUserPost:', err);
-        res.status(500).json({ message: 'An error occurred during signup', error: err.message });
     }
+    
 
-}
+]
 
